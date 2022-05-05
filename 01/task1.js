df758f0ce27745ae5815b8670111b795bbfab9b3
Logunovich <logunovich@gmail.com>
2022-05-05 11:30:58 +0200

    fix: fixed code style

--- 01/task1.js
+++ 01/task1.js
@@ -1,8 +1,11 @@
 const num1 = prompt('Введите первое число', '').replace(/,/, '.');
 const num2 = +prompt('Введите второе число', '');
 
-if(isNaN(Number(num1)) || !num1.trim() || !Number.isInteger(num2) || num2 < 2 || num2 > 36) {
+const checkNum1 = Boolean(isNaN(Number(num1)) || !num1.trim());
+const checkNum2 = Boolean(!Number.isInteger(num2) || num2 < 2 || num2 > 36);
+
+if(checkNum1 || checkNum2) {
   console.log('Некорректный ввод!');
 } else {
   console.log(Number(num1).toString(num2));
-}
+} 
--- 01/task2.js
+++ 01/task2.js
@@ -1,14 +1,14 @@
 const num1 = prompt('Введите первое число', '').replace(/,/, '.');
 
 const showAnswer = (n1, n2) => {
-  if(isNaN(Number(n2)) || !n2.trim() || Number(n2) === 0) {
+  if(isNaN(+n2) || !n2.trim() || +n2 === 0) {
     console.log('Некорректный ввод!');
   } else {
     console.log(`Ответ: ${n1+Number(n2)}, ${n1/Number(n2)}`);
   }
 }
 
-if(isNaN(Number(num1)) || !num1.trim()) {
+if(isNaN(+num1) || !num1.trim()) {
   console.log('Некорректный ввод!');
 } else {
   const num2 = prompt('Введите второе число', '').replace(/,/, '.');
